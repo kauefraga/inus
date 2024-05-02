75d48bcc77d5989c1e26e2f6efcd483b1480a341
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-05-01 22:07:06 -0300

    feat: gofiber hello inus

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	fmt.Println("Hello, Inus!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"Hello": "Inus!",
+		})
+	})
+
+	app.Listen(":3000")
 }

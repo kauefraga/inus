552eb2b3fd934ba8729c1beb9d206578a746d3f2
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-05-15 21:42:55 -0300

    feat: set database url depending on the environment and build it with environment variables

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,14 +2,30 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const connString string = "postgresql://docker:docker@localhost:5432/inus?sslmode=disable"
-
 func Connect() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connString := "postgresql://docker:docker@localhost:5432/inus?sslmode=disable"
+
+	if os.Getenv("APP_ENV") == "production" {
+		connString = fmt.Sprintf(
+			"user=%s password=%s host=aws-0-sa-east-1.pooler.supabase.com port=5432 dbname=postgres",
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+		)
+	}
+
 	db, err := sql.Open("postgres", connString)
 	if err != nil {
 		log.Fatal(err)
